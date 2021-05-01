20c4ba87d85ee60d4c27cee57eb24ff197d27c57
armando-sys <patrickarmando87@gmail.com>
2021-05-01 15:21:02 +0700

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -8,11 +8,13 @@ export default function test() {
             variant="small-caps"
             family="Arial, Helvetica, sans-serif"
             padding="0px 200px 0px 200px"
+            margin="20px 0px 0px 0px"
         />;
         <Paragraph value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla sem tortor, ut gravida velit efficitur in. Phasellus finibus, turpis eu suscipit varius, quam erat suscipit sapien, eget ultricies mi nunc id nunc. Quisque varius eu turpis vel egestas. Pellentesque placerat purus felis, id ornare tortor vestibulum ut. Ut aliquam bibendum volutpat. In accumsan dictum pellentesque. Cras ultrices scelerisque nibh vel rhoncus. Etiam nec magna vitae felis hendrerit porttitor ut pretium orci."
             size="16px"
             padding="0px 200px 0px 200px"
             align="justify"
-            margin="-10px 0px 0px 0px" />
+            margin="-10px 0px 0px 0px"
+        />;
     </>
 }
